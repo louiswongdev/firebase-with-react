12b53b35cba7b982a7d5fb1e0f0bc44b41f44148
Louis <louis@Louiss-Mac-mini.local>
2020-06-05 17:54:20 -0400

    Initial setup of files

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBhrG8xeBA7chJVqfuQzckO8eD3HzupafM',
+  authDomain: 'think-piece-live2.firebaseapp.com',
+  databaseURL: 'https://think-piece-live2.firebaseio.com',
+  projectId: 'think-piece-live2',
+  storageBucket: 'think-piece-live2.appspot.com',
+  messagingSenderId: '871030870059',
+  appId: '1:871030870059:web:129002d5f3fb559b7fda6e',
+};
+
+firebase.initializeApp(firebaseConfig);
+
+window.firebase = firebase;
+
+export default firebase;

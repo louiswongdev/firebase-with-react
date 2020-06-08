6b83d3557fb16d89177d3233b436f6a08d3fbb16
Louis <louis@Louiss-Mac-mini.local>
2020-06-07 20:43:07 -0400

    Setup route for PostPage. Also fixed issue with google oAuth logged in users who have changed name or profile image. Would first load the original Google profile info before Context state of user auth kicks in

--- src/components/CurrentUser.jsx
+++ src/components/CurrentUser.jsx
@@ -6,26 +6,32 @@ import { Link } from 'react-router-dom';
 
 const CurrentUser = ({ displayName, photoURL, email, createdAt, children }) => {
   return (
-    <section className="CurrentUser">
-      <div className="CurrentUser--profile">
-        {photoURL && <img src={photoURL} alt={displayName} />}
-        <div className="CurrentUser--information">
-          <Link to="/profile">
-            <h2>{displayName}</h2>
-          </Link>
-          <p className="email">{email}</p>
-          <p className="created-at">
-            {createdAt &&
-              new Date(createdAt.seconds * 1000).toLocaleDateString('en-US')}
-            {/* {moment(createdAt).calendar()} */}
-          </p>
-        </div>
-      </div>
-      <div>
-        <div>{children}</div>
-        <button onClick={signOut}>Sign Out</button>
-      </div>
-    </section>
+    <div>
+      {createdAt && (
+        <section className="CurrentUser">
+          <div className="CurrentUser--profile">
+            {photoURL && <img src={photoURL} alt={displayName} />}
+            <div className="CurrentUser--information">
+              <Link to="/profile">
+                <h2>{displayName}</h2>
+              </Link>
+              <p className="email">{email}</p>
+              <p className="created-at">
+                {createdAt &&
+                  new Date(createdAt.seconds * 1000).toLocaleDateString(
+                    'en-US',
+                  )}
+                {/* {moment(createdAt).calendar()} */}
+              </p>
+            </div>
+          </div>
+          <div>
+            <div>{children}</div>
+            <button onClick={signOut}>Sign Out</button>
+          </div>
+        </section>
+      )}
+    </div>
   );
 };
 
